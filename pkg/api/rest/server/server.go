d9a7a2e25764d6d49c958e720b5f7aabb047f742
ish <ish@innogrid.com>
2024-05-07 14:17:37 +0900

    api: rest: server: Show running information

--- pkg/api/rest/server/server.go
+++ pkg/api/rest/server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"fmt"
+	"github.com/cloud-barista/cm-cicada/common"
 	"github.com/cloud-barista/cm-cicada/pkg/api/rest/middlewares"
 	"github.com/cloud-barista/cm-cicada/pkg/api/rest/route"
+	"net"
+	"strings"
 
 	"github.com/cloud-barista/cm-cicada/lib/config"
 	_ "github.com/cloud-barista/cm-cicada/pkg/api/rest/docs" // Cicada Documentation
@@ -10,14 +14,57 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const (
+	infoColor   = "\033[1;34m%s\033[0m"
+	noticeColor = "\033[1;36m%s\033[0m"
+)
+
+const (
+	website = " https://github.com/cloud-barista/cm-cicada"
+)
+
+func getLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		logger.Println(logger.ERROR, true, err)
+		return ""
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	localIP := strings.Split(localAddr.String(), ":")
+	if len(localIP) == 0 {
+		logger.Println(logger.ERROR, true, "Failed to get local IP.")
+		return ""
+	}
+
+	return localIP[0]
+}
+
 func Init() {
 	e := echo.New()
 
 	e.Use(middlewares.CustomLogger())
 
+	// Hide Echo Banner
+	e.HideBanner = true
+
 	route.DAG(e)
 	route.RegisterSwagger(e)
 	route.RegisterUtility(e)
+
+	// Display API Docs Dashboard when server starts
+	endpoint := getLocalIP() + ":" + config.CMCicadaConfig.CMCicada.Listen.Port
+	apiDocsDashboard := " http://" + endpoint + "/" + strings.ToLower(common.ShortModuleName) + "/swagger/index.html"
+
+	fmt.Println("\n ")
+	fmt.Println(" CM-Cicada repository:")
+	fmt.Printf(infoColor, website)
+	fmt.Println("\n ")
+	fmt.Println(" API Docs Dashboard:")
+	fmt.Printf(noticeColor, apiDocsDashboard)
+	fmt.Println("\n ")
 
 	err := e.Start(":" + config.CMCicadaConfig.CMCicada.Listen.Port)
 	logger.Panicln(logger.ERROR, true, err)
